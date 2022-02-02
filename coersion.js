5d7e586ac1a78874b0625d6de2c9bfdeb73461a6
Luke Czoboriek <LC169782@protonmail.com>
2022-02-02 15:39:18 +0000

    added in examples of coersion and comparison differences

--- coersion.js
+++ coersion.js
@@ -1,4 +1,12 @@
 // Coersion is the changing of values from one data type to another
 
 let yearNumber = 1995;
-console.log(yearNumber);
+console.log(yearNumber + 1);
+console.log(yearNumber - 1);
+
+let yearString = "1995";
+console.log(yearString + 1);
+console.log(yearNumber == yearString); // same value string (digits to number)
+console.log(yearNumber === yearString); // false: same value but different type
+
+//All or most comparisons in js are ===, == allows for coersion of one or more operands
