362e4ac4d6cd6e93ed74d04cc59d1cfbc511b6e0
Luke Czoboriek <LC169782@protonmail.com>
2022-02-02 15:31:09 +0000

    added in coersion.js

---
+++ coersion.js
@@ -0,0 +1,4 @@
+// Coersion is the changing of values from one data type to another
+
+let yearNumber = 1995;
+console.log(yearNumber);
--- employeeScript.js
+++ employeeScript.js
@@ -6,4 +6,4 @@ var tim = "Employee " + name + " started on " + startingDate;
 console.log(tim);
 
 var tim2 = `Employee: ${name} started on ${startingDate} and earns £${salary} /year. Permament status: ${isPerm}`
-console.log(tim2)
+console.log(tim2) 
