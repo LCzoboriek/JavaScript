3ea6e88ec6356bff879db29766651448c9710942
Luke Czoboriek <LC169782@protonmail.com>
2022-02-02 15:24:08 +0000

    better string concat

--- employeeScript.js
+++ employeeScript.js
@@ -1,4 +1,9 @@
 var name = "Tim Smith";
 var startingDate = 2017;
+var salary = 2409.05;
+var isPerm = true;
 var tim = "Employee " + name + " started on " + startingDate;
 console.log(tim);
+
+var tim2 = `Employee: ${name} started on ${startingDate} and earns £${salary} /year. Permament status: ${isPerm}`
+console.log(tim2)
