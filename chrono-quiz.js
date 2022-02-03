eb47c349528ebc2072411ff5616c641fffacd09f
Luke Czoboriek <LC169782@protonmail.com>
2022-02-03 11:49:43 +0000

    added in functions and chrono quiz start, ran out of time for chrono quiz

---
+++ chrono-quiz.js
@@ -0,0 +1,11 @@
+var question1 = 'In what year did the Titanic sink on her maiden voyage?: '
+var question2 = 'In what year did WW1 begin?: '
+var question3 = 'In what year was the Qualification of Women act passed?: '
+var answer1 = '1912'
+var answer2 = '1914'
+var answer3 = '1918'
+
+for (var i = 1; i <= 3; i++) {
+    var guess = '';
+}
+
