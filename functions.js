3cb8cdf37b8594f1f07574702f31456aa7caa270
Luke Czoboriek <LC169782@protonmail.com>
2022-02-03 12:02:51 +0000

    added in functions for input and output

--- functions.js
+++ functions.js
@@ -14,4 +14,28 @@ function greet1(){
 }
 greet1();
 
+// Like this, without a return statement, we cannot persist output and pass it on. 
+var greet1 = greet1();
+console.log('greet1: ' + greet1); // Undefined
+
+function greet2(name){
+    console.log("Hello");
+    console.log(name);
+    console.log("How are you today?");
+}
+greet2("Luke");
+
+//Still cannot pass output to other parts of the program
+var result2 = greet2("Patty");
+console.log('greet2: ' + greet2("Patty")); //greet2: undefined
+
+//Function declaration
+//input and output
+function greet3(name){
+    return "Hello " + name + " how are you today?";
+}
+greet3("Peter");
+// no console output without passing the result of the function to CLG
+console.log(greet3("Peter"));
+
 
