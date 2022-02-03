3df4a3ae7312cc2c6b7fd3781f03ae6baf4e2db3
Luke Czoboriek <LC169782@protonmail.com>
2022-02-03 11:53:05 +0000

    added greet function

---
+++ functions.js
@@ -0,0 +1,17 @@
+console.log('Hello');
+console.log('World');
+console.log('How are you today?');
+
+// Essential
+
+// 1. function declaration
+// function declarations are loaded into memory at the start
+// no input, no output
+function greet1(){
+    console.log("Hello");
+    console.log("world");
+    console.log("How are you today?");
+}
+greet1();
+
+
