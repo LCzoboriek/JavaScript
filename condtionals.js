500660ed2728a4b6ef838280f3acea3e63cdc0c9
Luke Czoboriek <LC169782@protonmail.com>
2022-02-03 10:07:05 +0000

    added loops and conditionals files, and examples of a basic conditional

---
+++ condtionals.js
@@ -0,0 +1,14 @@
+// A conditional is a program flow structure that takes a boolean
+// expression which if true the block executes immediately after
+// ()
+// if FALSE skips that block and resumes normal oeprations
+// 
+// 1. No branches
+
+if(false){
+    console.log('code in the block is executed')
+}
+
+if(true){
+    console.log('code is now executed');
+}
